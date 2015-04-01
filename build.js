1396a887c7cadd72ddf119296f472fd06b475bf6
Gene Diaz <genediazjr@gmail.com>
2015-04-01 10:40:12 +0800

    add minify and switch to svg2png

--- build.js
+++ build.js
@@ -11,7 +11,8 @@ var fs = require('fs'),
   async = require('async'),
   chalk = require('chalk'),
   dir = require('node-dir'),
-  svg2png = require('svg-to-png'),
+  svg2png = require('svg2png'),
+  Imagemin = require('imagemin'),
   task = process.argv[3],
   srcDir = process.argv[2],
   srcPath = path.join(__dirname, srcDir),
@@ -108,54 +109,37 @@ dir.files(srcPath, function (err, files) {
         var filename = path.basename(file),
           pngFile = filename.replace('.svg', '.png');
         try {
-
           async.series([
 
             function (callback2) {
-              svg2png.convert(file, outPath).then(function () {
-                util.log(chalk.green(filename + ' converted ✓'));
-                callback2();
-              }, function (err) {
-                callback2(err);
+              svg2png(file, path.join(outPath, pngFile), 2, function (err) {
+                if (err) {
+                  callback2(err);
+                } else {
+                  util.log(chalk.green(filename + ' converted ✓'));
+                  callback2();
+                }
               });
             },
 
             function (callback2) {
               lwip.open(path.join(outPath, pngFile), function (err, image) {
-                image.resize(72, function (err, image) {
+                processImage(image, 72, path.join(outPath72, pngFile), function (err) {
                   if (err) {
                     callback2(err);
                   } else {
-                    image.writeFile(path.join(outPath72, pngFile), function (err) {
+                    util.log(chalk.green(filename + ' 72x72 resized ✓'));
+                    processImage(image, 36, path.join(outPath36, pngFile), function (err) {
                       if (err) {
                         callback2(err);
                       } else {
-                        util.log(chalk.green(filename + ' 72x72 resized ✓'));
-                        image.resize(36, function (err, image) {
+                        util.log(chalk.green(filename + ' 36x36 resized ✓'));
+                        processImage(image, 16, path.join(outPath16, pngFile), function (err) {
                           if (err) {
                             callback2(err);
                           } else {
-                            image.writeFile(path.join(outPath36, pngFile), function (err) {
-                              if (err) {
-                                callback2(err);
-                              } else {
-                                util.log(chalk.green(filename + ' 36x36 resized ✓'));
-                                image.resize(16, function (err, image) {
-                                  if (err) {
-                                    callback2(err);
-                                  } else {
-                                    image.writeFile(path.join(outPath16, pngFile), function (err) {
-                                      if (err) {
-                                        callback2(err);
-                                      } else {
-                                        util.log(chalk.green(filename + ' 16x16 resized ✓'));
-                                        callback2();
-                                      }
-                                    });
-                                  }
-                                });
-                              }
-                            });
+                            util.log(chalk.green(filename + ' 16x16 resized ✓'));
+                            callback2();
                           }
                         });
                       }
@@ -168,7 +152,6 @@ dir.files(srcPath, function (err, files) {
           ], function (err) {
             callback1(err);
           });
-
         } catch (err) {
           callback1(err);
         }
@@ -184,4 +167,40 @@ dir.files(srcPath, function (err, files) {
     });
   }
 });
+
+function processImage(image, resize, outpath, callback) {
+  image.clone(function (err, clone) {
+    if (err) {
+      callback(err);
+    } else {
+      clone.resize(resize, function (err, clone) {
+        if (err) {
+          callback(err);
+        } else {
+          clone.toBuffer('png', function (err, buffer) {
+            if (err) {
+              callback(err);
+            } else {
+              var imagemin = new Imagemin().src(buffer);
+              imagemin.use(Imagemin.optipng({optimizationLevel: 3}));
+              imagemin.run(function (err, files) {
+                if (err) {
+                  callback(err);
+                } else {
+                  fs.writeFile(outpath, files[0].contents, function (err) {
+                    if (err) {
+                      callback(err);
+                    } else {
+                      callback();
+                    }
+                  });
+                }
+              });
+            }
+          });
+        }
+      });
+    }
+  });
+}
 
